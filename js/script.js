fa4dd3b350d0a19dd20e704ae34b5c2b74a754f2
jessicawu2 <jessicauwu24@gmail.com>
2019-10-23 22:17:45 +0000

    work in progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+let words=
+$(".input").val();
+return 'words'.charAt(0);
 
 
 
-  
-  
-  
-  
+
+
+
 });
